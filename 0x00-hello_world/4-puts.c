f43fcf973ba9c84494f0c2d2c10f077aa5a3e463
Lesedi-Bunny <lesedibunny10@gmail.com>
2023-10-05 06:59:02 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * *main-this is the main puts function that will be utilised below
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-this a printf function that will be used below
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
