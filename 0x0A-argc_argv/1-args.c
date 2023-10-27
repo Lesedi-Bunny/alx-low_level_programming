4607c7aa009cf21726a11deb0a411fa09e5c7eb7
Lesedi-Bunny <lesedibunny10@gmail.com>
2023-10-27 07:01:11 -0700

    C arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
-#include <stdio.h>		
+#include <stdio.h>
 #include "main.h"
 /**
-  * main -this function prints the number of arguments passed to it		
-  * @argc: number of arguments		
-  * @argv: array of arguments		
-  *		
-  * Return: Always 0 (Success)		
-  */		
-int main(int argc, char *argv[])		
+  * main -this function prints the number of arguments passed to it
+  * @argc: number of arguments
+  * @argv: array of arguments
+  *
+  * Return: Always 0 (Success)
+  */
+int main(int argc, char *argv[])
 {
-	(void) argv; 
+	(void) argv;
 
 	printf("%d\n", argc - 1);
 	return (0);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,7 +5,7 @@
 /**
   * check_num - check - string there are digit
   * @str: array str
-  * 
+  *
   * Return: Always 0 (Success)
   */
 int check_num(char *str)
@@ -13,9 +13,9 @@ int check_num(char *str)
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str)) 
+	while (count < strlen(str))
 	{
-	if (!isdigit(str[count]))
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
@@ -38,11 +38,11 @@ int main(int argc, char *argv[])
 	int sum = 0;
 
 	count = 1;
-	while (count < argc) 
+	while (count < argc)
 	{
 	if (check_num(argv[count]))
 	{
-		str_to_int = atoi(argv[count]); 
+		str_to_int = atoi(argv[count]);
 		sum += str_to_int;
 	}
 	else
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 	}
 	count++;
 	}
-	printf("%d\n", sum); 
+	printf("%d\n", sum);
 	return (0);
 
 }
